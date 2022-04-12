e59b057edff3aee8d05e6ffe2241ca36e1c606a7
sireeshapuli <puli.sireesha1@gmail.com>
2022-04-12 17:11:18 -0500

    new changes

--- src/com/training/mars/MyClass.java
+++ src/com/training/mars/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first JAVA file");
-		System.out.println("New changes");
+		System.out.println("New Changes");
             
 	}
 
